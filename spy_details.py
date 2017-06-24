10126ebb6d8de1aee776651724cbadbb21c71ddb
sanjaymehra <sanjaykumar26596@gmail.com>
2017-06-25 01:21:42 +0530

    My first commit

---
+++ spy_details.py
@@ -0,0 +1,31 @@
+from datetime import datetime
+
+class Spy:
+
+    def __init__(self, name, salutation, age, rating):
+        self.name = name
+        self.salutation = salutation
+        self.age = age
+        self.rating = rating
+        self.is_online = True
+        self.chats = []
+        self.current_status_message = "Hey there! Im using spyChat!"
+
+
+class ChatMessage:
+
+    def __init__(self,message,sent_by_me):
+        self.message = message
+        self.time = datetime.now()
+        self.sent_by_me = sent_by_me
+
+spy = Spy('sanjay', 'Mr.', 20, 4.7)
+
+friend_one = Spy('Niharika', 'Mr.', 4.9, 20)
+friend_two = Spy('shreeya', 'Ms.', 4.39, 20)
+friend_three = Spy('shakti', 'Mr.', 4.39, 20)
+
+
+friends = [friend_one, friend_two,friend_three]
+
+
